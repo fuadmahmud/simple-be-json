ec8fc899079714f108637b4da6723959b4695617
fuadmahmud <fuadmahmud10@gmail.com>
2024-05-15 17:57:55 +0700

    feat: add vercel deployment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const cors = require('cors');
